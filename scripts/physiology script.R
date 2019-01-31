e4e9645ca7804b96e3e9e77cba7a524f75229c35
Pansa <pcecchin@my.hpu.edu>
2019-01-30 16:57:28 -1000

    Bargraphs of cell cts and chl

--- scripts/physiology script.R
+++ scripts/physiology script.R
@@ -12,10 +12,21 @@ str(data)
 data$displacement..ml=as.numeric(data$displacement..ml) #get displacement to be numeric
 str(data)
 
+data$cells.ml[24]<-1727500
+data$cells[24]<-3160670
+data$cells.ml[46]<-2515000
+data$cells[46]<-3539214
+data$cells.ml[70]<-1620000
+data$cells[70]<-2452176
+data$cells.ml[94]<-2358333.33333
+data$cells[94]<-4872019 
+#changed values of recounted cells in the matrix
+
 data$chla..ug.ml[24]<-3.704
 data$chla..ug.ml[94]<-4.081
 data$chla..ug.ml[50]<-3.538
 data$chla..ug.ml[53]<-4.579
+#changed values of abnormally high data after retesting
 
 data$chlc2..ug.ml[24]<-0.648
 data$chlc2..ug.ml[94]<-0.655
@@ -25,11 +36,9 @@ data$chlc2..ug.ml[53]<-0.616
 
 
 data$chla=as.numeric(data$chla..ug.ml*data$blastate..ml/data$surface.area..cm2)
-data$chla
 data$chlc2=as.numeric(data$chlc2..ug.ml*data$blastate..ml/data$surface.area..cm2)
-data$chlc2
 data$cells=as.numeric(data$cells.ml*data$blastate..ml/data$surface.area..cm2)
-data$cells
+
 #normalized the data for chla, chlc2, cell counts
 
 
@@ -77,41 +86,38 @@ Boxplot(cells~data$feeding,
      ylab="Cells/cm2",
      xlab="Feed")
 title("Cell Concentration", outer=TRUE)
-max(cells)
 
-max(chl) #this value is abnormal in the context of the plot
+library(plotrix)
+library(ggplot2)
 
-#below are edits to the data based on recounts 
-data2<-data #created a second data set based on the original where revised data will be stored. 
-data2$cells.ml[24]<-1727500
-data2$cells[24]<-3160670
-data2$cells.ml[46]<-2515000
-data2$cells[46]<-3539214
-data2$cells.ml[70]<-1620000
-data2$cells[70]<-2452176
-data2$cells.ml[94]<-2358333.33333
-data2$cells[94]<-4872019 #changed values of recounted cells in the matrix
-cells2<-data2$cells
-cells.ml2<-data2$cells.ml
+#Chlorophyll by treatment
+chl.mean<-aggregate(chl~data$treatment + data$symbiont.genotype, data, mean)
+colnames(chl.mean)<-c("treatment", "sym", "mean")
+chl.SE<-aggregate(chl~data$treatment + data$symbiont.genotype, data, std.error)
+colnames(chl.SE)<-c("treatment", "sym", "SE")
+chl.df<-cbind(chl.mean, chl.SE[c(0,3)])
 
+fig.chl<-ggplot(chl.df, aes(x=as.factor(treatment), y=mean, fill=sym))+
+  geom_bar(position=position_dodge(0.9), stat="identity")+
+  geom_errorbar(aes(ymin=mean-SE, ymax=mean+SE), width=0.2, position=position_dodge(0.9))+ #add error bars
+  theme(legend.position="bottom", plot.title=element_text(hjust = 0.5))+
+  scale_y_continuous(limits=c(0,16.5))+ #set yaxis 
+  labs(title="Chlorophyll Content by Treatment", position="center", x="Treatment code", y="Chlorophyll (ug/cm2)", legend="Symbiont Genotype")+ #add labels
+  scale_fill_discrete(name="Symbiont genotype") #add legend label
+fig.chl
+#make a plot of chlorophyll by treatment
 
-Boxplot(cells2~data2$treatment, 
-        col="lightblue", 
-        ylab="symbiont cells/cm2", 
-        xlab="Treatment code",
-        main="Symbiont Cells by Treatment revised") #created plots using second dataset
+#cells by treatment
+cells.mean<-aggregate(cells~data$treatment + data$symbiont.genotype, data, mean)
+colnames(cells.mean)<-c("treatment", "sym", "mean")
+cells.SE<-aggregate(cells~data$treatment + data$symbiont.genotype, data, std.error)
+colnames(cells.SE)<-c("treatment", "sym", "SE")
+cells.df<-cbind(cells.mean, cells.SE[c(0,3)])
 
-par(mfrow=c(1,3), oma=c(10,4,2,1), pty="sq") #mar is bottom, left, top, right, oma specifies where title
-Boxplot(cells2~data2$temperature,
-        col="010203",
-        ylab="Cells/cm2",
-        xlab="Temperature")
-Boxplot(cells2~data2$light,
-        col="lightyellow",
-        ylab="Cells/cm2",
-        xlab="Light")
-Boxplot(cells2~data2$feeding,
-        col="334421",
-        ylab="Cells/cm2",
-        xlab="Feed")
-title("Cell Concentration revised", outer=TRUE)
+fig.cells<-ggplot(cells.df, aes(x=as.factor(treatment), y=mean, fill=sym))+
+  geom_bar(position=position_dodge(0.9), stat="identity")+
+  geom_errorbar(aes(ymin=mean-SE, ymax=mean+SE), width=0.2, position=position_dodge(0.9))+ #add error bars
+  theme(legend.position="bottom", plot.title=element_text(hjust = 0.5))+
+  labs(title="Cell Concentration by Treatment", position="center", x="Treatment code", y="Cells", legend="Symbiont Genotype")+ #add labels
+  scale_fill_discrete(name="Symbiont genotype") #add legend label
+ fig.cells
